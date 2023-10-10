44382a2af4b4521e14deb689a3e9df37da527464
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-10-10 17:12:31 +0900

    집집

--- Git_test_KWH_My/src/main/java/com/itwillbs/test/controller/KSJController.java
+++ Git_test_KWH_My/src/main/java/com/itwillbs/test/controller/KSJController.java
@@ -1,5 +1,10 @@
 package com.itwillbs.test.controller;
 
 public class KSJController {
-	
+	public static void main(String[] args) {
+		for(int i = 0; i <= 100; i++) {
+			
+			System.out.println("헤헤 28분남았다~!");
+		}
+	}
 }
