ab92210abfc502b37770b98217e8efa7fd8ef3ec
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-10-10 17:12:01 +0900

    KSJController 수정

--- Git_test_KWH_My/src/main/java/com/itwillbs/test/controller/KSJController.java
+++ Git_test_KWH_My/src/main/java/com/itwillbs/test/controller/KSJController.java
@@ -2,4 +2,7 @@ package com.itwillbs.test.controller;
 
 public class KSJController {
 	
+	public static void main(String[] args) {
+		System.out.println("하이");
+	}
 }
--- Git_test_KWH_My/src/main/java/com/itwillbs/test/controller/LYTController.java
+++ Git_test_KWH_My/src/main/java/com/itwillbs/test/controller/LYTController.java
@@ -9,5 +9,7 @@ public class LYTController {
 	public static void main(String[] args) {
 		System.out.println("응애");
 	}
+	
+	
 
 }
